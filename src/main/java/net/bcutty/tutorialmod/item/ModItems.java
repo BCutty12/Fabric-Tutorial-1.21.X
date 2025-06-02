94d3e7dede2f2a5276690198c12b8f1dfcb85f44
Bcutty <bcuttruff@outlook.com>
2025-06-02 17:23:13 -0400

    added Pink Garnet and Raw Pink Garnet items to minecraft

--- src/main/java/net/bcutty/tutorialmod/item/ModItems.java
+++ src/main/java/net/bcutty/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.bcutty.tutorialmod.item;
 
 import net.bcutty.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -19,6 +21,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
         });
     }
 }
