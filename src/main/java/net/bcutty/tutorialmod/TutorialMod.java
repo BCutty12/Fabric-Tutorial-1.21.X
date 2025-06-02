ef76f5b4f4066012153f3781fcbbb22305ddc2db
Bcutty <bcuttruff@outlook.com>
2025-06-02 16:02:45 -0400

    add test

---
+++ src/main/java/net/bcutty/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.bcutty.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// Test
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
