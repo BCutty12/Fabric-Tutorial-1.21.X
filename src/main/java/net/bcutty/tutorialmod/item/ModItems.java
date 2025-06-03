503a03e448ffbd8391733a97c007215b9128a042
Bcutty <bcuttruff@outlook.com>
2025-06-02 23:53:30 -0400

    infinerite ingot

--- src/main/java/net/bcutty/tutorialmod/item/ModItems.java
+++ src/main/java/net/bcutty/tutorialmod/item/ModItems.java
@@ -12,6 +12,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item INFINERITE_INGOT = registerItem("infinerite_ingot", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -22,6 +23,7 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(PINK_GARNET);
             fabricItemGroupEntries.add(RAW_PINK_GARNET);
+            fabricItemGroupEntries.add(INFINERITE_INGOT);
         });
     }
 }
