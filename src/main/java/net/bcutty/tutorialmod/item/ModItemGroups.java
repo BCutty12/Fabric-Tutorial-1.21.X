08536f452131641b8546446b476404d22f669afa
Bcutty <bcuttruff@outlook.com>
2025-06-03 20:25:14 -0400

    new groups

---
+++ src/main/java/net/bcutty/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,42 @@
+package net.bcutty.tutorialmod.item;
+
+import net.bcutty.tutorialmod.TutorialMod;
+import net.bcutty.tutorialmod.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup PINK_GARNET_ITEMs_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "pink_garnet_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.PINK_GARNET);
+                        entries.add(ModItems.RAW_PINK_GARNET);
+                    }).build());
+    public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "pink_garnet_blocks"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.PINK_GARNET_BLOCK))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_blocks"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.PINK_GARNET_BLOCK);
+                        entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+                    }).build());
+    public static final ItemGroup INFINERITE_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "infinerite"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.INFINERITE_INGOT))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.infinerite"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.INFINERITE_INGOT);
+                        entries.add(ModBlocks.INFINERITE_ORE);
+                    }).build());
+
+    public static void registerItemGroups(){
+        TutorialMod.LOGGER.info("Registering Item Groups for" + TutorialMod.MOD_ID);
+    }
+}
