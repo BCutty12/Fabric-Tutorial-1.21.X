93cd41bed5f06dc2cd72cd94dd83444f8070efd9
Bcutty <bcuttruff@outlook.com>
2025-06-05 21:38:48 -0400

    added a bunch of stuff and forgot to save LOL

--- src/main/java/net/bcutty/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/bcutty/tutorialmod/item/ModItemGroups.java
@@ -11,13 +11,14 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
-    public static final ItemGroup PINK_GARNET_ITEMs_GROUP = Registry.register(Registries.ITEM_GROUP,
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID, "pink_garnet_items"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
                     .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_items"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
+                        entries.add(ModItems.CHISEL);
                     }).build());
     public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID, "pink_garnet_blocks"),
@@ -26,6 +27,9 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.PINK_GARNET_BLOCK);
                         entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+                        entries.add(ModBlocks.PINK_GARNET_ORE);
+                        entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+                        entries.add(ModBlocks.MAGIC_BLOCK);
                     }).build());
     public static final ItemGroup INFINERITE_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID, "infinerite"),
--- src/main/java/net/bcutty/tutorialmod/item/ModItems.java
+++ src/main/java/net/bcutty/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.bcutty.tutorialmod.item;
 
 import net.bcutty.tutorialmod.TutorialMod;
+import net.bcutty.tutorialmod.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -14,6 +15,7 @@ public class ModItems {
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
     public static final Item INFINERITE_INGOT = registerItem("infinerite_ingot", new Item(new Item.Settings()));
     public static final Item RAW_INFINERITE = registerItem("raw_infinerite", new Item(new Item.Settings()));
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
